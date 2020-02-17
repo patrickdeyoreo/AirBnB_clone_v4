539c10e073440fd19d9b8d924745d22541fcc49c
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-17 12:06:23 -0800

    Added .js; CORS won't work

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(() => {
+  const amenities = {};
+  $('input:checkbox').click(() => {
+    $(this).each(() => {
+      if (this.checked) {
+        amenities[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amenities[$(this).data('id')];
+      }
+    });
+    if (Object.values(amenities).length > 0) {
+      $('.amenities h4').text(Object.values(amenities).join(', '));
+    } else {
+      $('.amenities h4').html('&nbsp');
+    }
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', (data, status) => {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+});
