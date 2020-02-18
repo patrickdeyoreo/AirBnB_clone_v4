015228a606f11004e29dc2120ee8a2b3ff63728a
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-17 23:09:32 -0800

    Adding task 4 js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -23,16 +23,43 @@ $(document).ready(() => {
   });
   $.ajax({
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
-    type: 'post',
+    type: 'POST',
     contentType: 'application/json',
-    data: {},
+    processData: false,
+    data: '{}',
     dataType: 'json',
-    success: (data) => {
-      for (const place of Object.values(data)) {
-        console.log(place);
-        const article = $('<article>');
-        article.append('<div>', { class: 'information' }).add('<div>', { class: 'max_guest' }).add('<i>', { class: 'fa fa-users fa-3x', 'aria-hidden': 'true' }, `${place.max_guest} Guests`);
-      }
+    success: (places) => {
+      $.ajax({
+        url: 'http://0.0.0.0:5001/api/v1/users/',
+        type: 'GET',
+        success: (users) => {
+          const owners = {};
+          for (const user of Object.values(users)) {
+            owners[user.id] = [user.first_name, user.last_name].join(' ');
+          }
+          for (const place of Object.values(places)) {
+            console.log(place);
+            const article = $('<article>');
+            const title = $('<div class="title">');
+            title.append(`<h2>${place.name}</h2>`);
+            title.append(`<div class="price_by_night">$${place.price_by_night}</div>`);
+            const information = $('<div class="information">');
+            const maxGuest = $('<div class="max_guest">').add(`<i class="fa fa-users fa-3x" aria-hidden="true"></i>${place.max_guest} Guests`);
+            const numberRooms = $('<div class="number_rooms">').add(`<i class="fa fa-users fa-3x" aria-hidden="true"></i>${place.number_rooms} Bedrooms`);
+            const numberBathrooms = $('<div class="number_bathrooms">').add(`<i class="fa fa-users fa-3x" aria-hidden="true"></i>${place.number_bathrooms} Bathrooms`);
+            const owner = $('<div class="user">').add(`<strong>Owner: ${owners[place.user_id]}</strong>`);
+            const description = $('<div class="description">').add(`<p>${place.description}</p>`);
+            information.append(maxGuest);
+            information.append(numberRooms);
+            information.append(numberBathrooms);
+            article.append(title);
+            article.append(information);
+            article.append(owner);
+            article.append(description);
+            $('.places').append(article);
+          }
+        }
+      });
     }
   });
 });
