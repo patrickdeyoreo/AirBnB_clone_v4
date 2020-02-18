8f640059c7571948fecc04665603dc4eda892f41
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-18 08:49:59 -0800

    Resetting search results between searches (duuhhh)

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -26,36 +26,37 @@ $(function () {
       contentType: 'application/json',
       data: JSON.stringify({ amenities: Object.keys(amenities) }),
       success: (places) => {
+        const section = $('section.places');
+        section.empty();
         $.get('http://0.0.0.0:5001/api/v1/users/', (users) => {
           for (const user of Object.values(users)) {
             users[user.id] = `${user.first_name} ${user.last_name}`;
           }
           for (const place of Object.values(places)) {
-            const section = $('.places');
             const article = $('<article>');
             const name = $('<div class="title">');
             name.append(`<h2>${place.name}</h2>`);
             name.append(`<div class="price_by_night">$${place.price_by_night}</div>`);
+            article.append(name);
             const info = $('<div class="information">');
             const maxGuest = $('<div class="max_guest">');
             maxGuest.append('<i class="fa fa-users fa-3x" aria-hidden="true"></i>');
             maxGuest.append(`${place.max_guest} Guests`);
+            info.append(maxGuest);
             const numRooms = $('<div class="number_rooms">');
             numRooms.append('<i class="fa fa-bed fa-3x" aria-hidden="true"></i>');
             numRooms.append(`${place.number_rooms} Bedrooms`);
+            info.append(numRooms);
             const numBaths = $('<div class="number_bathrooms">');
             numBaths.append('<i class="fa fa-bath fa-3x" aria-hidden="true"></i>');
             numBaths.append(`${place.number_bathrooms} Bathrooms`);
-            info.append(maxGuest);
-            info.append(numRooms);
             info.append(numBaths);
+            article.append(info);
             const user = $('<div class="user">');
             user.append(`<strong>Owner: ${users[place.user_id]}</strong>`);
+            article.append(user);
             const desc = $('<div class="description">');
             desc.append(`<p>${place.description}</p>`);
-            article.append(name);
-            article.append(info);
-            article.append(user);
             article.append(desc);
             section.append(article);
           }
